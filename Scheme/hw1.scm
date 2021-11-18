cbae762b7eb19bf1ea0e6989d18e0820b8dc5cd2
Ryan Lynch <lynchryan2@gmail.com>
2021-11-18 18:02:35 +0000

    Reverting

--- Scheme/hw1.scm
+++ Scheme/hw1.scm
@@ -1,8 +1,5 @@
 #lang mzscheme
 
-(define before-seq
-  (λ (xs ys)
-    (if (null? xs) ys)))
 
 ;;; Individual Tests for Q1
 
@@ -14,6 +11,7 @@
       (cond
         ((equal? (before-seq '(a b) '(x y z 1 2 3 4 a b c d a a b)) '(z 4 a)) 0)
         (else 1))))
+
 
 
 ;;; Test 2
@@ -34,25 +32,3 @@
       (cond
         ((equal? (before-seq '() '(j k l m n)) '(j k l m n)) 0)
         (else 1))))
-
-
-;;; Test 4
-;;; Given '(t) & '(a b t u v t t)
-;;; Returns 0 if correctly evaluated to '(b v t)
-(define test4-before-seq
-  (λ ()
-      (cond
-        ((equal? (before-seq '(t) '(a b t u v t t)) '(b v t)) 0)
-        (else 1))))
-
-
-;;; Test All
-;;; Runs all individual test cases
-;;; Returns a list with an element for each test run.
-;;; Each element will be a 1 or 0. 1 signifies failure, 0 signifies success.
-(define test-before-seq
-  (λ ()
-    (list (test1-before-seq)
-          (test2-before-seq)
-          (test3-before-seq)
-          (test4-before-seq))))
